00539bf1819fd47ffdf0ba12761445d1752b0049
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-05-14 20:38:49 +0700

    bug fixes

---
+++ Warp'er_SE Project/Assets/Scripts/MovingPlatform2.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform2 : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    public float speed;
+    public ButtonScript bs;
+
+    private Vector3 nextPos;
+    private bool isMoving = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (bs._isActivate == true || (Input.GetKeyDown(KeyCode.F) && bs._isPlayer == true))
+        {
+            check();
+        }
+        if (isMoving == true)
+        {
+            move();
+        }
+    }
+
+    void move()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        if (transform.position == nextPos)
+        {
+            isMoving = false;
+        }
+    }
+
+    void check()
+    {
+        //determine destination
+        if (transform.position == PointA.position)
+        {
+            nextPos = PointB.position;
+        }
+        else if (transform.position == PointB.position)
+        {
+            nextPos = PointA.position;
+        }
+        isMoving = true;
+    }
+}
