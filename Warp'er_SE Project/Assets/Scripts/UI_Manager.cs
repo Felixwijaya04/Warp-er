4afbb3ef19ded2fac72f088075f0a809d2d338c5
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-05-18 12:14:52 +0700

    add pause menu

--- Warp'er_SE Project/Assets/Scripts/UI_Manager.cs
+++ Warp'er_SE Project/Assets/Scripts/UI_Manager.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
+    public void Start()
+    {
+        Time.timeScale = 1.0f;
+    }
     public void playTutorial()
     {
         SceneManager.LoadScene("Tutorial");
@@ -16,6 +20,18 @@ public class UI_Manager : MonoBehaviour
         SceneManager.LoadScene("Level 1");
     }
 
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void pause()
+    {
+        Time.timeScale = 0f;
+    }
+    public void resume()
+    {
+        Time.timeScale = 1f;
+    }
     public void exitGame()
     {
         Application.Quit();
